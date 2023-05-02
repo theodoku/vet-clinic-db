3e57b570746926698d1d9d13b15b9386e166e30a
theophilus doku <theodoku89@gmail.com>
2023-05-02 15:12:50 +0000

    Modified LICENSE file, README file, data.sql, queries.sql, schema.sql

--- schema.sql
+++ schema.sql
@@ -1,12 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id BIGSERIAL NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    date_of_birth DATE,
-    escape_attempts INT NOT NULL,
-    neutered BOOLEAN,
-    weight_kg DECIMAL NOT NULL,
-    PRIMARY KEY(id)
+    name varchar(100)
 );
-
