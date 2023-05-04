4e5ea765bc8e53848a4d0015d57509070e35bb74
theophilus doku <theodoku89@gmail.com>
2023-05-04 08:21:03 +0000

    Modified data.sql, queries.sql and schema.sql file

--- schema.sql
+++ schema.sql
@@ -11,3 +11,22 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+CREATE TABLE owners (
+ id BIGSERIAL PRIMARY KEY,
+ full_name VARCHAR (150),
+ age INT
+);
+
+CREATE TABLE species (
+ id BIGSERIAL PRIMARY KEY,
+ name VARCHAR(150)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE  animals
+ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
