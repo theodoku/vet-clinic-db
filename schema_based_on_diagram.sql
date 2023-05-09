c962213318966425d3d9604e5641b2f3cbaa17be
Lornakaboro <lornakaboro@gmail.com>
2023-05-09 20:42:26 +0300

    Add invoice and medical history and invoice tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,3 +23,33 @@ CREATE TABLE invoices (
  CONSTRAINT fk_medical_history_id FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 CREATE INDEX idx_medical_history_id ON invoices (medical_history_id);
+
+CREATE TABLE treatments (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    type VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+    id BIGSERIAL NOT NULL,
+    unit_price DECIMAL NOT NULL,
+    Quantity INT,
+    total_price DECIMAL,
+    invoice_id INT REFERENCES invoices (id),
+    treatment_id INT REFERENCES treatments (id),
+    PRIMARY KEY(id)
+);
+
+CREATE INDEX idx_invoice_items_invoice_treatment_id 
+ON invoice_items (invoice_id, treatment_id);
+
+CREATE TABLE medical_history_treatments (
+  medical_history_id BIGINT NOT NULL REFERENCES medical_histories(id),
+  treatment_id BIGINT NOT NULL REFERENCES treatments(id),
+  PRIMARY KEY(medical_history_id, treatment_id),
+  CONSTRAINT fk_medical_history_treatments_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
+  CONSTRAINT fk_medical_history_treatments_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id)
+);
+CREATE INDEX idx_medical_history_treatments_history_treatment 
+ON medical_history_treatments (medical_history_id, treatment_id);
